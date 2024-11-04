9d0f557b6b0f956263af03cf3745c3d07ba7c124
hyperoot <hyperoot.tech@proton.me>
2024-11-04 06:32:06 +0000

    fix: minor tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,37 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+import rehypeExternalLinks from "rehype-external-links";
 
-import icon from 'astro-icon';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), mdx(), icon({
-    iconDir: "src/assets/icons"
-  })]
+  prefetch: true,
+  markdown: {
+    shikiConfig: {
+      theme: "css-variables",
+      defaultColor: false,
+    },
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+        },
+      ],
+    ],
+  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    mdx(),
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+  ],
 });
